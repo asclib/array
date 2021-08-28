c5432f9eedfd5e4d11946102b4182af3e0f82f2a
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2021-08-29 02:08:55 +0900

    Add array_list

---
+++ lib/array_list/array_list.h
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define Array(T)				\
+  struct Array {				\
+    T *contents;				\
+    uint32_t size;				\
+    uint32_t capacity;				\
+  }
+
+#define array_init(self)						\
+  ((self)->size = 0, (self)->capacity = 0, (self)->contents = NULL)
+
+#define array_new()				\
+  { NULL, 0, 0}
+
+#define array_get(self, index)			\
+  (assert((uint32_t) index < (self)->size), &(self)->contents[index])
+
+#define array_head(self) array_get(self, 0)
+
+#define array_last(self) array_get(self, (self)->size - 1)
+
+#define array_clear(self) ((self)->size = 0)
+
+#define array_insert(self, index, element)				\
+  array__splice((VoidArray *) (self), sizeof((self)->contents), index, 0, 1, &element)
+
+typedef Array(void) VoidArray;
+
+static inline void array__reserve(VoidArray *self, size_t element_size, uint32_t new_capacity) {
+  if (new_capacity > self->capacity) {
+    if (self->contents) {
+      self->contents = realloc(self->contents, new_capacity * element_size);
+    } else {
+      self->contents = malloc(new_capacity * element_size);
+    }
+    self->capacity = new_capacity;
+  }
+}
+
+static inline void array__grow(VoidArray *self, size_t margin, size_t element_size) {
+  size_t new_size = self->size + margin;
+  if (new_size > self->capacity) {
+    size_t new_capacity = self->capacity * 2;
+    if (new_capacity < 8) {
+      new_capacity = 8;
+    }
+    if (new_capacity < new_size) {
+      new_capacity = new_size;
+    }
+    array__reserve(self, element_size, new_capacity);
+  }
+}
+
+static inline void array__splice(VoidArray *self,
+				 size_t element_size,
+				 uint32_t index,
+				 uint32_t old_count,
+				 uint32_t new_count,
+				 const void *elements) {
+  uint32_t new_size = self->size + new_count - old_count;
+  uint32_t old_end = index + old_count;
+  uint32_t new_end = index + new_count;
+  assert(old_end <= self->size);
+
+  array__reserve(self, element_size, new_size);
+
+  char *contents = (char *) self->contents;
+  if (self->size > old_end) {
+    memmove(contents + new_end * element_size,
+	    contents + old_end * element_size,
+	    (self->size - old_end) * element_size);
+  }
+  if (new_count > 0) {
+    if (elements) {
+      memcpy((contents + index * element_size),
+	     elements,
+	     new_count * element_size);
+    } else {
+      memset((contents + index * element_size),
+	     0,
+	     new_count * element_size);
+    }
+  }
+  self->size += new_count - old_count;
+}
